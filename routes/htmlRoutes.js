982720514cbac12f5a070e5deb37015f815f36c0
beausimpson <simpson.beau@gmail.com>
2019-01-14 13:02:13 -0600

    added code sequel prod vs dev code, set up schema.sql

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,6 +12,7 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
+  // prettier-ignore
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
